fb3add07d396d64f2512f82208282f8abafc5dd8
AlexBangs7 <alexandrabangs@gmail.com>
2026-09-03 10:26:39 -0700

    Fix typo in slurm scripts

--- Project2_Part2/cutadapt.sh
+++ Project2_Part2/cutadapt.sh
@@ -7,7 +7,7 @@
 #SBATCH --job-name=cutadapt
 #SBATCH --time=2-0:00:00
 
-cd ../Project2_Part1
+cd ../Project2_QAA
 /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
  pixi run cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
  -o ../Project2_Part2/cutadapt_output/SRR25630297_1_adapted.fastq -p ../Project2_Part2/cutadapt_output/SRR25630297_2_adapted.fastq \
--- Project2_Part2/trimmomatic.sh
+++ Project2_Part2/trimmomatic.sh
@@ -7,7 +7,7 @@
 #SBATCH --job-name=trimmomatic
 #SBATCH --time=2-0:00:00
 
-cd ../Project2_Part1/
+cd ../Project2_QAA/
 /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
  pixi run trimmomatic PE ../Project2_Part2/cutadapt_output/SRR25630297_1_adapted.fastq ../Project2_Part2/cutadapt_output/SRR25630297_2_adapted.fastq \
  ../Project2_Part2/trimmomatic_output/SRR25630297_1_paired.fastq ../Project2_Part2/trimmomatic_output/SRR25630297_1_unpaired.fastq ../Project2_Part2/trimmomatic_output/SRR25630297_2_paired.fastq ../Project2_Part2/trimmomatic_output/SRR25630297_2_unpaired.fastq \
