8f628e69c40f19a144a55a3ae29f1390be37a035
AlexBangs7 <alexandrabangs@gmail.com>
2026-09-03 11:24:15 -0700

    Upload zipping.sh

---
+++ Project2_Part2/trimmomatic_output/zipping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=bgmp                    # REQUIRED: which account to use
+#SBATCH --partition=bgmp                  # REQUIRED: which partition to use
+#SBATCH --cpus-per-task=8                 # optional: number of cpus, default is 1
+#SBATCH --job-name=zipping
+#SBATCH --time=2-0:00:00
+
+# /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
+#  pigz SRR25630381_1_paired.fastq SRR25630381_2_paired.fastq 
+ 
+/usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
+ pigz SRR25630297_1_paired.fastq SRR25630297_2_paired.fastq
