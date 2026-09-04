e85f6f0ffd3fb7a6628ef15fe3e1d532efa29bee
AlexBangs7 <alexandrabangs@gmail.com>
2026-09-03 20:10:08 -0700

    Upload htseq-count.sh

--- Project2_Part3/htseq-count.sh
+++ Project2_Part3/htseq-count.sh
@@ -7,29 +7,34 @@
 #SBATCH --job-name=htseq-counting            # optional: job name
 #SBATCH --time=2-0:00:00
 
-SAM=/projects/bgmp/abangs/bioinfo/Bi621/PS/AlexBangs7-Bi621-PS8/SAMparser.py
+SAM=/projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/SAMparser.py
 
 GFF=/projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/campylomormyrus.gff
 
-6CM=/projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/STARalign_output/6CM_Aligned.out.sam
+SIXCM=/projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/STARalign_output/6CM_Aligned.out.sam
 
 ADULT=/projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/STARalign_output/adult_Aligned.out.sam
 
 cd ../Project2_QAA/
-/usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run $SAM -i $6CM
+
+# 6CM files
 
 /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run $SAM -i $ADULT
+ pixi run $SAM -i $SIXCM
 
 /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run htseq-count --stranded=yes -i Parent -c $6CM $GFF
+ pixi run htseq-count --stranded=yes -i Parent $SIXCM $GFF > /projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/CcoxCrh_comrhy60_EO_6cm_1_htseqcounts_forstranded.txt
 
 /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run htseq-count --stranded=yes -i Parent -c $ADULT $GFF
+ pixi run htseq-count --stranded=reverse -i Parent $SIXCM $GFF > /projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/CcoxCrh_comrhy60_EO_6cm_1_htseqcounts_revstranded.txt
 
-/usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run htseq-count --stranded=reverse -i Parent -c $6CM $GFF
+# ADULT files
 
-/usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
- pixi run htseq-count --stranded=reverse -i Parent -c $ADULT $GFF
+# /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
+#  pixi run $SAM -i $ADULT
+
+# /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
+#  pixi run htseq-count --stranded=yes -i Parent $ADULT $GFF > /projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/CcoxCrh_comrhy112_EO_adult_2_htseqcounts_forstranded.txt
+
+# /usr/bin/time -f "Command being timed: %C\nPercent of CPU this job got: %P\nElapsed (wall clock) time (h:mm:ss or m:ss): %E\nMaximum resident set size (kbytes): %M\nExit status: %x" \
+#  pixi run htseq-count --stranded=reverse -i Parent $ADULT $GFF > /projects/bgmp/abangs/bioinfo/Bi623/AlexBangs7-Bi623-Project-2/Project2_Part3/CcoxCrh_comrhy112_EO_adult_2_htseqcounts_revstranded.txt
